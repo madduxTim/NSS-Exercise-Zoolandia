88f45f187083a319636e51c77e4eca0e098fee80
madduxTim <timothymaddux@gmail.com>
2016-07-31 18:54:05 -0500

    Habitats created

---
+++ Habitats/Steppe.cs
@@ -0,0 +1,10 @@
+namespace Zoolandia.Habitats
+{
+    public class Steppe : Habitat
+    {
+        public Steppe(string name) : base(name)
+        {
+            this.isUnderWater = false;
+        }
+    }
+}
